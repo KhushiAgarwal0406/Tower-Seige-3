2fcee042ec01ad09e37686d1c048ce7ef5f0b953
KhushiAgarwal0406 <77060817+KhushiAgarwal0406@users.noreply.github.com>
2021-02-27 19:00:52 +0530

    Add files via upload

---
+++ blue box.js
@@ -0,0 +1,15 @@
+class Box extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+      
+        fill(130, 237, 237);
+        super.display();
+     
+  }
+}
---
+++ yellow box.js
@@ -0,0 +1,15 @@
+class Box3 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+      
+    fill(239,242,72);
+    super.display();
+      
+  }
+}
